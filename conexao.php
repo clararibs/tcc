6520d6a930d6b77ab070c89f06c97dda103bd3b0
@clararibs <clararibsdutra@gmail.com>
2025-10-07 14:53:46 -0300

    fizemos a conexão com o banco

--- conexao.php
+++ conexao.php
@@ -1,12 +1,20 @@
 <?php
-define('HOST', 'localhost:3307');
-define('USER', 'ROOT');
-define('PASSWORD', '');
-define('DB', 'bdpessoa');
-$conn = new mysqli (HOST, USER, PASSWORD, DB);
-if($conn --> connect_error){
-    die("falha na conexão:" .$conn->connect_error);
+// ===== Dados do banco =====
+$host = "3.150.114.68";        // Geralmente localhost
+$usuario = "tcc_hedone";   // Seu usuário do banco
+$senha = "1hN^83}";       // Sua senha do banco
+$banco = "hedone_db";       // Nome do banco de dados
 
+// ===== Criando a conexão =====
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+// ===== Checando a conexão =====
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+} else {
+    echo "Conexão realizada com sucesso!";
 }
-//echo "conexão realizada com sucesso"
+
+// ===== Fechando a conexão =====
+$conn->close();
 ?>
